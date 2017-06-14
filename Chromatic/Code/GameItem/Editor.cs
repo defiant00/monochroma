95befe8f3961580635776b8b1459a69906f0ab50
Aaron Mady <defiant00@gmail.com>
2017-06-14 17:21:35 -0400

    selecting, editing and rotating tiles

--- Chromatic/Code/GameItem/Editor.cs
+++ Chromatic/Code/GameItem/Editor.cs
@@ -1,15 +1,28 @@
 ﻿using Chromatic.Code.Renderable;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Chromatic.Code.GameItem
 {
-	class Editor : IGameItem
+    class Editor : IGameItem
     {
+        enum State
+        {
+            Main,
+            SelectTile
+        }
+        State CurState;
+
         Chromatic Game;
         bool _Remove = false;
         Map Map;
         Vector2 Offset;
+
+        List<Sprite> TileSprites;
+        Sprite CurrentTile;
 
         public bool Remove { get { return _Remove; } }
 
@@ -20,64 +33,147 @@ namespace Chromatic.Code.GameItem
 
         public void Draw(GameTime gameTime)
         {
-			Vector2 floorOffset = Offset.Floor();
-			Game.GraphicsDevice.SetRenderTarget(Game.SpriteTarget);
-			Game.GraphicsDevice.Clear(Game.BackColor);
-            Game.SpriteBatch.Begin();
-            Map.Draw(Game.SpriteBatch, floorOffset);
-            Game.SpriteBatch.End();
+            Vector2 floorOffset = Offset.Floor();
+            if (CurState == State.Main)
+            {
+                Game.GraphicsDevice.SetRenderTarget(Game.SpriteTarget);
+                Game.GraphicsDevice.Clear(Game.BackColor);
+                Game.SpriteBatch.Begin();
+                Map.Draw(Game.SpriteBatch, floorOffset);
+                Game.SpriteBatch.End();
 
-			Game.GraphicsDevice.SetRenderTarget(Game.LightTarget);
-			Game.GraphicsDevice.Clear(Color.Gray);
-			Game.RadialEffect.Draw(new Rectangle(0, 0, 800, 600), new Color(1f, 0, 0), Offset);
-			Game.RadialEffect.Draw(new Rectangle(300, 0, 800, 600), new Color(0, 0.4f, 0), Offset);
-			Game.RadialEffect.Draw(new Rectangle(150, 150, 800, 600), new Color(0, 0, 1f), Offset);
+                Game.GraphicsDevice.SetRenderTarget(Game.LightTarget);
+                Game.GraphicsDevice.Clear(Color.White);
+                //Game.RadialEffect.Draw(new Rectangle(0, 0, 800, 600), new Color(1f, 0, 0), Offset);
 
-			Game.SpriteBatch.Begin();
-			Game.RectangleSprite.Draw(Game.SpriteBatch, Game.SpriteMapTex, new Rectangle(Offset.ToPoint(), new Point(300, 120)), Color.White);
-			Game.SpriteBatch.End();
+                Game.GraphicsDevice.SetRenderTarget(Game.InterfaceTarget);
+                Game.GraphicsDevice.Clear(Color.Transparent);
+                Game.SpriteBatch.Begin();
+                Game.RectangleSprite.Draw(Game.SpriteBatch, Game.SpriteMapTex, new Rectangle(0, 0, 1280, 40), Color.Black);
+                CurrentTile.Draw(Game.SpriteBatch, Game.SpriteMapTex, Vector2.Zero);
+                Game.SpriteBatch.End();
+            }
+            else if (CurState == State.SelectTile)
+            {
+                Game.GraphicsDevice.SetRenderTarget(Game.SpriteTarget);
+                Game.GraphicsDevice.Clear(Game.BackColor);
 
-			Game.GraphicsDevice.SetRenderTarget(Game.InterfaceTarget);
-			Game.GraphicsDevice.Clear(Color.Transparent);
+                Game.SpriteBatch.Begin();
+                foreach (var t in TileSprites) { t.Draw(Game.SpriteBatch, Game.SpriteMapTex, Vector2.Zero); }
+                Game.SpriteBatch.End();
+
+                Game.GraphicsDevice.SetRenderTarget(Game.LightTarget);
+                Game.GraphicsDevice.Clear(Color.White);
+
+                Game.GraphicsDevice.SetRenderTarget(Game.InterfaceTarget);
+                Game.GraphicsDevice.Clear(Color.Transparent);
+            }
         }
 
         public void LoadContent()
         {
             Map = new Map(Game, 42, 24, "t_gggg");
-		}
+            CurrentTile = new Sprite(Game.SpriteMap, "t_wwww") { Position = new Vector2(17, 17) };
 
-		public void UnloadContent()
-        {
+            TileSprites = FitSprites("t_");
         }
+
+        private List<Sprite> FitSprites(string prefix)
+        {
+            var sprites = new List<Sprite>();
+            var areas = new List<Rectangle>();
+            areas.Add(new Rectangle(0, 0, 1280, 720));
+            foreach (var tKey in Game.SpriteMap.Keys.Where(k => k.StartsWith(prefix)))
+            {
+                var spr = Game.SpriteMap[tKey];
+                var outerSize = new Point(spr.Frames[0].Rectangle.Width + 1, spr.Frames[0].Rectangle.Height + 1);
+                bool placed = false;
+                for (int i = 0; i < areas.Count && !placed; i++)
+                {
+                    var area = areas[i];
+                    if (outerSize.X <= area.Width && outerSize.Y <= area.Height)
+                    {
+                        sprites.Add(new Sprite(Game.SpriteMap, tKey) { Position = area.Location.ToVector2() + (spr.Frames[0].Rectangle.Size.ToVector2() / 2).Floor() });
+
+                        areas.RemoveAt(i);
+                        if (outerSize.Y < area.Height)
+                        {
+                            areas.Insert(i, new Rectangle(area.X, area.Y + outerSize.Y, area.Width, area.Height - outerSize.Y));
+                        }
+                        if (outerSize.X < area.Width)
+                        {
+                            areas.Insert(i, new Rectangle(area.X + outerSize.X, area.Y, area.Width - outerSize.X, outerSize.Y));
+                        }
+                        placed = true;
+                    }
+                }
+                if (!placed)
+                {
+                    throw new Exception(tKey + " won't fit!");
+                }
+            }
+            return sprites;
+        }
+
+        public void UnloadContent() { }
 
         public void Update(GameTime gameTime)
         {
             double ms = gameTime.ElapsedGameTime.TotalMilliseconds;
             Map.Update(ms);
+            foreach (var t in TileSprites) { t.Update(ms); }
 
-			Vector2 floorOffset = Offset.Floor();
-			int offX = (int)floorOffset.X;
-			int offY = (int)floorOffset.Y;
-
-            
-
-            int mx = Game.Input.Mouse.X - offX;
-            int my = Game.Input.Mouse.Y - offY;
-            int tx = mx / 32;
-            int ty = my / 32;
-
-            if (Game.Input.Mouse.LeftButton == InputState.ButtonState.Pressed && mx > -1 && my > -1 && tx < Map.Width && ty < Map.Height)
+            if (CurState == State.Main)
             {
-                var t = Map.Tiles[tx, ty];
-                t.Play("t_wwww");
-            }
+                Vector2 floorOffset = Offset.Floor();
+                int offX = (int)floorOffset.X;
+                int offY = (int)floorOffset.Y;
 
-            float change = (float)ms / 5;
-            var kbd = Keyboard.GetState();
-            if (kbd.IsKeyDown(Keys.W)) { Offset.Y += change; }
-            if (kbd.IsKeyDown(Keys.S)) { Offset.Y -= change; }
-            if (kbd.IsKeyDown(Keys.A)) { Offset.X += change; }
-            if (kbd.IsKeyDown(Keys.D)) { Offset.X -= change; }
+                int mx = Game.Input.Mouse.X - offX;
+                int my = Game.Input.Mouse.Y - offY;
+                int tx = mx / 32;
+                int ty = my / 32;
+
+                if (Game.Input.Mouse.Y < 40)
+                {
+                    if (Game.Input.Mouse.X < 34)
+                    {
+                        if (Game.Input.Mouse.LeftButton == InputState.ButtonState.JustPressed)
+                        {
+                            CurState = State.SelectTile;
+                        }
+                        else if (Game.Input.Mouse.RightButton == InputState.ButtonState.JustPressed)
+                        {
+                            CurrentTile.Rotation = (CurrentTile.Rotation + MathHelper.PiOver2) % MathHelper.TwoPi;
+                        }
+                    }
+                }
+                else if (Game.Input.Mouse.LeftButton == InputState.ButtonState.Pressed && mx > -1 && my > -1 && tx < Map.Width && ty < Map.Height)
+                {
+                    var t = Map.Tiles[tx, ty];
+                    t.Play(CurrentTile.Animation);
+                    t.Rotation = CurrentTile.Rotation;
+                }
+
+                float change = (float)ms / 5;
+                var kbd = Keyboard.GetState();
+                if (kbd.IsKeyDown(Keys.W)) { Offset.Y += change; }
+                if (kbd.IsKeyDown(Keys.S)) { Offset.Y -= change; }
+                if (kbd.IsKeyDown(Keys.A)) { Offset.X += change; }
+                if (kbd.IsKeyDown(Keys.D)) { Offset.X -= change; }
+            }
+            else if (CurState == State.SelectTile)
+            {
+                if (Game.Input.Mouse.LeftButton == InputState.ButtonState.JustPressed)
+                {
+                    var t = TileSprites.FirstOrDefault(s => s.Contains(Game.Input.Mouse.X, Game.Input.Mouse.Y));
+                    if (t != null)
+                    {
+                        CurrentTile.Play(t.Animation);
+                        CurState = State.Main;
+                    }
+                }
+            }
         }
     }
 }
--- Chromatic/Code/Renderable/Sprite.cs
+++ Chromatic/Code/Renderable/Sprite.cs
@@ -6,73 +6,80 @@ using System.Collections.Generic;
 
 namespace Chromatic.Code.Renderable
 {
-	public class Sprite
-	{
-		Dictionary<string, SpriteData> AllData;
-		SpriteData CurrentData;
+    public class Sprite
+    {
+        Dictionary<string, SpriteData> AllData;
+        SpriteData CurrentData;
 
-		string _Animation;
-		public string Animation { get { return _Animation; } }
-		public Vector2 Position;
-		public float Z;
-		public float Rotation;
+        string _Animation;
+        public string Animation { get { return _Animation; } }
+        public Vector2 Position;
+        public float Z;
+        public float Rotation;
 
-		int CurrFrame;
-		double TimeLeft;
-		Vector2 Origin;
+        int CurrFrame;
+        double TimeLeft;
+        Vector2 Origin;
 
-		public Sprite(Dictionary<string, SpriteData> data, string animation, Random random = null)
-		{
-			AllData = data;
-			Play(animation, random);
-			Origin = (CurrentData.Frames[0].Rectangle.Size.ToVector2() / 2).Floor();
-		}
+        public Sprite(Dictionary<string, SpriteData> data, string animation, Random random = null)
+        {
+            AllData = data;
+            Play(animation, random);
+            Origin = (CurrentData.Frames[0].Rectangle.Size.ToVector2() / 2).Floor();
+        }
 
-		public void Play(string animation, Random random = null)
-		{
-			_Animation = animation;
-			CurrentData = AllData[_Animation];
-			if (random == null || CurrentData.Frames.Length < 2) { Reset(); }
-			else
-			{
-				CurrFrame = random.Next(CurrentData.Frames.Length);
-				TimeLeft = random.NextDouble() * CurrentData.Frames[CurrFrame].DisplayTime;
-			}
-		}
+        public bool Contains(int x, int y)
+        {
+            Vector2 tl = Position - Origin;
+            Rectangle r = CurrentData.Frames[0].Rectangle;
+            return !(x < tl.X || y < tl.Y || x > (tl.X + r.Width) || y > (tl.Y + r.Height));
+        }
 
-		public void Draw(SpriteBatch spriteBatch, Texture2D texture, Vector2 offset)
-		{
-			var f = CurrentData.Frames[CurrFrame];
-			spriteBatch.Draw(texture, Position + offset, f.Rectangle, Color.White, Rotation + f.Rotation, Origin, 1, f.Effects, Z);
-		}
+        public void Play(string animation, Random random = null)
+        {
+            _Animation = animation;
+            CurrentData = AllData[_Animation];
+            if (random == null || CurrentData.Frames.Length < 2) { Reset(); }
+            else
+            {
+                CurrFrame = random.Next(CurrentData.Frames.Length);
+                TimeLeft = random.NextDouble() * CurrentData.Frames[CurrFrame].DisplayTime;
+            }
+        }
 
-		public void Draw(SpriteBatch spriteBatch, Texture2D texture, Rectangle destRect, Color color)
-		{
-			var f = CurrentData.Frames[CurrFrame];
-			spriteBatch.Draw(texture, destRect, f.Rectangle, color);
-		}
+        public void Draw(SpriteBatch spriteBatch, Texture2D texture, Vector2 offset)
+        {
+            var f = CurrentData.Frames[CurrFrame];
+            spriteBatch.Draw(texture, Position + offset, f.Rectangle, Color.White, Rotation + f.Rotation, Origin, 1, f.Effects, Z);
+        }
 
-		public void Update(double ms)
-		{
-			if (CurrentData.Frames.Length > 1)
-			{
-				TimeLeft -= ms;
-				while (TimeLeft < 0)
-				{
-					CurrFrame++;
-					if (CurrFrame >= CurrentData.Frames.Length)
-					{
-						CurrFrame = CurrentData.LoopIndex;
-					}
-					TimeLeft += CurrentData.Frames[CurrFrame].DisplayTime;
-				}
-			}
-		}
+        public void Draw(SpriteBatch spriteBatch, Texture2D texture, Rectangle destRect, Color color)
+        {
+            var f = CurrentData.Frames[CurrFrame];
+            spriteBatch.Draw(texture, destRect, f.Rectangle, color);
+        }
 
-		public void Reset()
-		{
-			CurrFrame = 0;
-			TimeLeft = CurrentData.Frames[0].DisplayTime;
-		}
-	}
+        public void Update(double ms)
+        {
+            if (CurrentData.Frames.Length > 1)
+            {
+                TimeLeft -= ms;
+                while (TimeLeft < 0)
+                {
+                    CurrFrame++;
+                    if (CurrFrame >= CurrentData.Frames.Length)
+                    {
+                        CurrFrame = CurrentData.LoopIndex;
+                    }
+                    TimeLeft += CurrentData.Frames[CurrFrame].DisplayTime;
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            CurrFrame = 0;
+            TimeLeft = CurrentData.Frames[0].DisplayTime;
+        }
+    }
 }
